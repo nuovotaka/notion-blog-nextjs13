bad46807640873ea2a2c8e3dd735531d25cefd88
tak n <tkn.infomail@gmail.com>
2023-01-02 08:06:33 +0900

    Remove path from server-constants.js

--- app/server-constants.js
+++ app/server-constants.js
@@ -10,7 +10,6 @@ const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_G_ADS_ID = process.env.NEXT_PUBLIC_G_ADS_ID
 const NEXT_PUBLIC_SITE_TITLE = 'dev.NUOVOTAKA'
 const NEXT_PUBLIC_SITE_DESCRIPTION = '時々更新ブログ'
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
@@ -21,6 +20,5 @@ module.exports = {
   NEXT_PUBLIC_G_ADS_ID,
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
