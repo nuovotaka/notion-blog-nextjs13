93717b8498f87e13f3b2818d4b7d2bc70a9194d9
tak n <tkn.infomail@gmail.com>
2023-01-15 09:40:27 +0900

    change logo image compress

--- pages/api/og-image.tsx
+++ pages/api/og-image.tsx
@@ -5,7 +5,7 @@ import { Post } from '../../lib/notion/interfaces'
 
 export const config = { runtime: 'experimental-edge' }
 
-const logoNT = '<svg width="100%" height="100%" viewBox="0 0 128 128" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;"><g id="logo"><g><path d="M128,9.788c0,-5.402 -4.386,-9.788 -9.788,-9.788l-108.424,0c-5.402,0 -9.788,4.386 -9.788,9.788l0,108.424c0,5.402 4.386,9.788 9.788,9.788l108.424,0c5.402,0 9.788,-4.386 9.788,-9.788l0,-108.424Z" style="fill:#303aa5;"/><path d="M7.961,112.175l0,-68.436c0,0 2.502,-11.265 9.727,-16.778c5.405,-4.125 10.119,-6.918 20.065,-7.159c10.811,-0.261 17.128,3.628 22.779,9.036c5.652,5.409 7.663,13.775 7.911,14.901c0.449,2.04 0,61.655 0,61.655l-14.797,6.781l0,-68.436c0,0 -1.843,-9.745 -7.691,-12.215c-5.847,-2.47 -12.366,-2.451 -16.287,0.516c-3.921,2.967 -7.046,8.322 -7.046,11.699c0,3.377 0,61.655 0,61.655l-14.661,6.781Z" style="fill:#fb3f1c;"/><path d="M80.309,43.739l11.191,0.051l0.339,-19.362l10.034,-4.638l4.88,0l0,23.949l15.217,0l-4.51,10.096l-10.707,0l0,51.622l-14.914,6.718l0,-58.34l-15.497,0l3.967,-10.096Z" style="fill:#fb3f1c;"/></g></g></svg>'
+const logoNT = '<svg viewBox="0 0 128 128" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421"><path d="M128 9.788C128 4.386 123.614 0 118.212 0H9.788C4.386 0 0 4.386 0 9.788v108.424C0 123.614 4.386 128 9.788 128h108.424c5.402 0 9.788-4.386 9.788-9.788V9.788Z" style="fill:#303aa5"/><path d="M7.961 112.175V43.739s2.502-11.265 9.727-16.778c5.405-4.125 10.119-6.918 20.065-7.159 10.811-.261 17.128 3.628 22.779 9.036 5.652 5.409 7.663 13.775 7.911 14.901.449 2.04 0 61.655 0 61.655l-14.797 6.781V43.739s-1.843-9.745-7.691-12.215c-5.847-2.47-12.366-2.451-16.287.516-3.921 2.967-7.046 8.322-7.046 11.699v61.655l-14.661 6.781ZM80.309 43.739l11.191.051.339-19.362 10.034-4.638h4.88v23.949h15.217l-4.51 10.096h-10.707v51.622l-14.914 6.718v-58.34H76.342l3.967-10.096Z" style="fill:#fb3f1c"/></svg>'
 
 const ApiOgImage = async function(req: NextRequest) {
   if (req.method !== 'GET') {
@@ -127,7 +127,7 @@ const ApiOgImage = async function(req: NextRequest) {
                 overflow: 'hidden',
               }}
             >
-              ✏️ {NEXT_PUBLIC_SITE_TITLE}
+              {NEXT_PUBLIC_SITE_TITLE}
             </div>
             <div
               style={{
